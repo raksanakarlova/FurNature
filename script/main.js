7dc6699f8a50855ce9f6d3ffe72fcfb0175694e4
Raksana Karlova <raksana.karlova@gmail.com>
2024-05-16 21:26:22 +0400

    Создание контента в карточках товара из базы данных

--- script/main.js
+++ script/main.js
@@ -1,12 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-	const btnConsulting = document.querySelector('.btn-consulting');
-	const modal = document.querySelector('.modal');
-	const overlay = document.querySelector('.overlay');
-	const btnClosedForm = document.querySelector('.btn-closed');
-
-	const forms = document.querySelectorAll('form');
-
-	// Создание карточек с преимуществами с помощью классов:
+	// Создание карточек-преимуществ с помощью классов:
 	class AdvantagesCard {
 		constructor(src, alt, title, descSelector, description, parentSelector, ...classes) {
 			this.src = src;
@@ -14,21 +7,22 @@ window.addEventListener('DOMContentLoaded', () => {
 			this.title = title;
 			this.descSelector = descSelector;
 			this.description = description;
-			this.classes = classes;
+			this.parentSelector = parentSelector;
 			this.parent = document.querySelector(parentSelector);
-		}
+			this.classes = classes;
+		};
 
 		render() {
-			const element = document.createElement('div');
+			const elem = document.createElement('div');
 
 			if(this.classes.length === 0) {
-				this.element = 'advantages-card';
-				element.classList.add(this.element);
+				this.elem = 'advantages-card';
+				elem.classList.add(this.elem);
 			} else {
-				this.classes.forEach(className => element.classList.add(className))
-			}
+				this.classes.forEach(className => elem.classList.add(className));
+			};
 
-			element.innerHTML = `
+			elem.innerHTML = `
 					<div class="advantages-content">
 						<div class="advantages-icon">
 							<img src=${this.src} alt=${this.alt}>
@@ -39,7 +33,7 @@ window.addEventListener('DOMContentLoaded', () => {
 					</div>
 			`;
 
-			this.parent.append(element);
+			this.parent.append(elem);
 		};
 	};
 
@@ -70,224 +64,194 @@ window.addEventListener('DOMContentLoaded', () => {
 		'.advantages-container'
 	).render();
 
-	// Создание карточек товара с помощью классов:
+	// Реализация работы классов:
 	class ProductCard {
 		constructor(
-			src, src2, src3, src4, alt, 
-			description, price, 
-			text, text2, text3, text4,
-			clrSelec, clrSelec2, clrSelec3, clrSelec4, 
+			mainimg, scndimg, thrdimg, frthimg,
+			altimg, desc, price, 
+			maintxt, scndtxt, thrdtxt, frthtxt,
+			mainslctr, scndslctr, thrdslctr, frthslctr,
 			parentSelector, ...classes
 		) {
-			this.src = src;
-			this.src2 = src2;
-			this.src3 = src3;
-			this.src4 = src4;
-			this.alt = alt;
-			this.description = description;
+			this.mainimg = mainimg;
+			this.scndimg = scndimg;
+			this.thrdimg = thrdimg;
+			this.frthimg = frthimg;
+
+			this.altimg = altimg;
+			this.desc = desc;
 			this.price = price;
-			this.text = text;
-			this.text2 = text2;
-			this.text3 = text3;
-			this.text4 = text4;
-			this.clrSelec = clrSelec;
-			this.clrSelec2 = clrSelec2;
-			this.clrSelec3 = clrSelec3;
-			this.clrSelec4 = clrSelec4;
-			this.classes = classes;
+
+			this.maintxt = maintxt;
+			this.scndtxt = scndtxt;
+			this.thrdtxt = thrdtxt;
+			this.frthtxt = frthtxt;
+
+			this.mainslctr = mainslctr;
+			this.scndslctr = scndslctr;
+			this.thrdslctr = thrdslctr;
+			this.frthslctr = frthslctr;
+
+			this.parentSelector = parentSelector;
 			this.parent = document.querySelector(parentSelector);
-		}
+
+			this.classes = classes;
+		};
 
 		render() {
-			const element = document.createElement('div');
-			if(this.classes.length === 0) {
-				this.element = 'card-container';
-				element.classList.add(this.element);
-			} else {
-				this.classes.forEach(className => element.classList.add(className));
-			};
+			const elem = document.createElement('div');
 
-			element.innerHTML = `
-					<div class="product-info-content">
-						<p class="descr">${this.description}</p>
-						<p class="price">${this.price}</p>
-						<div class="dec-line"></div>
-						<div class="colors">
-							<p class="title-color">
-								Цвет: <span class="name-color">${this.text}</span>
-											<span class="name-color hide-color">${this.text2}</span>
-											<span class="name-color hide-color">${this.text3}</span>
-											<span class="name-color hide-color">${this.text4}</span>
-							</p>
-							<div class="select-content">
-								<div class="select">
-									<div class="color ${this.clrSelec}"></div>
-								</div>
-								<div class="select hide-select">
-									<div class="color ${this.clrSelec2}"></div>
-								</div>
-								<div class="select hide-select">
-									<div class="color ${this.clrSelec3}"></div>
-								</div>
-								<div class="select hide-select">
-									<div class="color ${this.clrSelec4}"></div>
-								</div>
-							</div>
-						</div>
-							<button class="btn-add">Добавить в корзину</button>
-					</div>
-						<div>
-							<img class="picture" src=${this.src} alt=${this.alt}>
-							<img class="picture hide-picture" src=${this.src2} alt=${this.alt}>
-							<img class="picture hide-picture" src=${this.src3} alt=${this.alt}>
-							<img class="picture hide-picture" src=${this.src4} alt=${this.alt}>
-						</div>
+			if(this.classes.length === 0) {
+				this.elem = 'card-container';
+				elem.classList.add(this.elem);
+			} else {
+				this.classes.forEach(className => elem.classList.add(className));
+			}
+
+			elem.innerHTML = `
+				<div class="product-info-content">
+                    
+					<p class="descr">${this.desc}</p>
+          <p class="price">${this.price}</p>
+          <div class="dec-line"></div>
+          <div class="colors">
+            <p class="title-color">
+              Цвет: <span class="name-color">${this.maintxt}</span>
+                    <span class="name-color hide-color">${this.scndtxt}</span>
+                    <span class="name-color hide-color">${this.thrdtxt}</span>
+                    <span class="name-color hide-color">${this.frthtxt}</span>
+          	</p>
+            <div class="select-content">
+              <div class="select">
+                <div class="color ${this.mainslctr}"></div>
+              </div>
+              <div class="select hide-select">
+                <div class="color ${this.scndslctr}"></div>
+              </div>
+              <div class="select hide-select">
+                <div class="color ${this.thrdslctr}"></div>
+              </div>
+              <div class="select hide-select">
+                <div class="color ${this.frthslctr}"></div>
+              </div>
+          	</div>
+        	</div>
+            <button class="btn-add">Добавить в корзину</button>
+        </div>
+        <div>
+          <img class="picture" src=${this.mainimg} alt=${this.altimg}>
+          <img class="picture hide-picture" src=${this.scndimg} alt=${this.altimg}>
+          <img class="picture hide-picture" src=${this.thrdimg} alt=${this.altimg}>
+          <img class="picture hide-picture" src=${this.frthimg} alt=${this.altimg}>
+
+        </div>
 			`;
-			this.parent.append(element);
+			this.parent.append(elem);
 		};
 	};
 
-	new ProductCard(
-		'img/firstCard/main.png',
-		'img/firstCard/pink.png',
-		'img/firstCard/purple.png',
-		'img/firstCard/sea.png',
-		'product',
-		'Удобный стул с элегантным дизайном, идеальный для отдыха и работы',
-		'1.399 RUB',
-		'Голубой',
-		'Розовый',
-		'Фиолетовый',
-		'Зелёный',
-		'blue',
-		'pink',
-		'purple',
-		'green',
-		'.product-info-container'
-	).render();
+	const getResource = async (url) => {
+		const result = await fetch(url);
 
-	new ProductCard(
-		'img/secondCard/main.png',
-		'img/secondCard/brown.png',
-		'img/secondCard/blue.png',
-		'img/secondCard/olive.png',
-		'product',
-		'Эргономичный стул с современным стилем, обеспечивает комфорт и поддержку при сидении',
-		'2.399 RUB',
-		'Зелёный',
-		'Коричневый',
-		'Синий',
-		'Оливковый',
-		'green2',
-		'brown',
-		'sea',
-		'olive',
-		'.product-info-container'
-	).render();
+		if(!result.ok) {
+			throw new Error(`Couldn't fetch ${url}, status: ${result.status}`);
+		};
 
-	new ProductCard(
-		'img/thirdCard/main.png',
-		'img/thirdCard/grey.png',
-		'img/thirdCard/purple.png',
-		'img/thirdCard/orange.png',
-		'product',
-		'Стильный табурет для комфортного сидения в любом интерьере',
-		'1.199 RUB',
-		'Красный',
-		'Серый',
-		'Фуксия',
-		'Оранжевый',
-		'red',
-		'grey',
-		'fuchsia',
-		'light-brown',
-		'.product-info-container'
-	).render();
+		return result.json();
+	};
 
-	new ProductCard(
-		'img/fourthCard/main.png',
-		'img/fourthCard/red.png',
-		'img/fourthCard/pink.png',
-		'img/fourthCard/green.png',
-		'product',
-		'Этот стул — ваш верный спутник  в мире сидения',
-		'3.199 RUB',
-		'Чёрный',
-		'Оранжевый',
-		'Розовый',
-		'Салатовый',
-		'black',
-		'orange',
-		'pink2',
-		'light-green',
-		'.product-info-container'
-	).render();
+	getResource('http://localhost:3000/cards')
+		.then(data => {
+			data.forEach(({
+				mainimg, scndimg, thrdimg, frthimg,
+				altimg, desc, price,
+				maintxt, scndtxt, thrdtxt, frthtxt,
+				mainslctr, scndslctr, thrdslctr, frthslctr
+			}) => {
+				new ProductCard(
+					mainimg, scndimg, thrdimg, frthimg,
+					altimg, desc, price,
+					maintxt, scndtxt, thrdtxt, frthtxt,
+					mainslctr, scndslctr, thrdslctr, frthslctr,
+					'.product-info-container'
+				).render();
+			});
 
-	// Реализация работы табов:
-	const titleTabs = document.querySelectorAll('.title-product');
-	const widgets = document.querySelectorAll('.widget');
-	const contentTabs = document.querySelectorAll('.card-container');
-	const parentTabs = document.querySelector('.product-title-content');
-	
-	function hideTabContent () {
-		titleTabs.forEach(el => {
-			el.classList.remove('active');
+			setupTabs(); // устанавливаю обработчик после создания карточек
 		});
 
-		contentTabs.forEach((el) => {
-			el.style.display = 'none'
+	function setupTabs() {
+		// Реализация работы табов:
+		const titles = document.querySelectorAll('.title-product');
+		const widgets = document.querySelectorAll('.widget');
+		const contents = document.querySelectorAll('.card-container') // обновляю селектор для новых элементов
+		const parentTab = document.querySelector('.product-title-content');
+
+		function hideTabContent() {
+			titles.forEach(el => el.classList.remove('active'));
+			widgets.forEach(el => el.style.visibility = 'hidden');
+			
+			contents.forEach(el => {
+				el.classList.add('hide-content');
+				el.classList.remove('show-content');
+			});
+		};
+
+		function showTabContent(i = 0) {
+			titles[i].classList.add('active');
+			widgets[i].style.visibility = 'visible';
+
+			contents[i].classList.add('show-content');
+			contents[i].classList.remove('hide-content');
+		};
+
+		hideTabContent();
+		showTabContent();
+
+		parentTab.addEventListener('click', (event) => {
+			event.preventDefault();
+			const target = event.target;
+
+			if(target && target.classList.contains('title-product')) {
+				titles.forEach((el, index) => {
+					if(target == el) {
+						hideTabContent();
+						showTabContent(index);
+					};
+				});
+				};
 		});
 
-		widgets.forEach(el => {
-			el.style.visibility = 'hidden';
+		// Реализация изменений цвета, названий и изображений продукта:
+		contents.forEach(element => {
+			const colors = element.querySelectorAll('.color');
+			const selects = element.querySelectorAll('.select');
+			const colorNames = element.querySelectorAll('.name-color');
+			const imgs = element.querySelectorAll('.picture');
+
+			colors.forEach((color, index) => {
+				color.addEventListener('click', () => {
+					selects.forEach(select => select.classList.add('hide-select'));
+					selects[index].classList.remove('hide-select');
+
+					colorNames.forEach(name => name.classList.add('hide-color'))
+					colorNames[index].classList.remove('hide-color');
+
+					imgs.forEach(img => img.classList.add('hide-picture'));
+					imgs[index].classList.remove('hide-picture');
+				});
+			});
 		});
 	};
 
-	function showTabContent (i = 0) {
-		titleTabs[i].classList.add('active');
-		widgets[i].style.visibility = 'visible';
-		contentTabs[i].style.display = 'flex';
-	}
+	// Modal Form:
+	const btnConsulting = document.querySelector('.btn-consulting');
+	const modal = document.querySelector('.modal');
+	const overlay = document.querySelector('.overlay');
+	const btnClosedForm = document.querySelector('.btn-closed');
 
-	hideTabContent();
-	showTabContent(); 
+	const forms = document.querySelectorAll('form');
 
-	parentTabs.addEventListener('click', (event) => {
-		event.preventDefault();
-		const target = event.target;
-
-		if(target && target.classList.contains('title-product')) {
-			titleTabs.forEach((el, index) => {
-				if(target == el) {
-					hideTabContent();
-					showTabContent(index);
-				};
-			});
-		}
-	});
-
-	// Реализация изменения цвета, названия и изображения продукта:
-	contentTabs.forEach(tab => {
-		const colors = tab.querySelectorAll('.color');
-		const selects = tab.querySelectorAll('.select');
-		const colorNames = tab.querySelectorAll('.name-color');
-		const images = tab.querySelectorAll('.picture');
-
-		colors.forEach((color, i) => {
-			color.addEventListener('click', () => {
-				selects.forEach(select => select.classList.add('hide-select'));
-				selects[i].classList.remove('hide-select');
-
-				colorNames.forEach(name => name.classList.add('hide-color'));
-				colorNames[i].classList.remove('hide-color');
-
-				images.forEach(image => image.classList.add('hide-picture'));
-				images[i].classList.remove('hide-picture');
-			});
-		});
-	});
-
-	// Modal Form
 	function openModal() {
 		modal.classList.remove('inactive');
 		modal.classList.add('active');
@@ -308,7 +272,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	btnClosedForm.addEventListener('click', closeModal);
 
-	// Реализация работы с сервером:
+	// Реализация отправки формы с данными на сервер:
 	const message = {
 		loading: 'img/icons/spinner.svg',
 		succsess: 'Спасибо!',
@@ -318,10 +282,22 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	forms.forEach(el => {
-		postData(el);
+		bindPostData(el);
 	});
 
-	function postData(form) {
+	const postData = async (url, data) => {
+		const result = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: data
+		});
+
+		return await result.json();
+	};
+
+	function bindPostData(form) {
 		form.addEventListener('submit', (event) => {
 			event.preventDefault();
 
@@ -335,19 +311,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			const formData = new FormData(form);
 	
-			const object = {};
-			formData.forEach(function(value, key){
-				object[key] = value;
-			});
+			const json = JSON.stringify(Object.fromEntries(formData.entries()))
 
-			fetch('server.php', {
-				method: 'POST',
-				headers: {
-					'Content-type': 'application/json'
-				},
-				body: JSON.stringify(object)
-			})
-			.then(data => data.text())
+			postData('http://localhost:3000/requests', json)
 			.then(data => {
 				console.log(data);
 				showThanksModal(message.succsess, message.succsess2);
@@ -360,6 +326,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	};
 
+	// Оповещение пользователя:
 	function showThanksModal(message, msg) {
 		const prevModalDialog = document.querySelector('.form-container');
 		prevModalDialog.classList.add('inactive');
@@ -387,6 +354,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			prevModalDialog.classList.add('active');
 			prevModalDialog.classList.remove('inactive');
 			closeModal();
-		}, 2000)
+		}, 1000)
 	};
 });
